48f4c8ee737122cd8b4633f12ea0ef794e248cf5
abuzarcodes <abuzar200496@gmail.com>
2024-05-11 17:54:42 +0530

    added done feature

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,20 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaRegFile } from "react-icons/fa";
 import { MdDeleteForever } from "react-icons/md";
 import { MdEdit } from "react-icons/md";
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
 
-function Card({refrence, data}) {
+function Card({ refrence, data }) {
+  const [checked, setChecked] = useState(false);
   return (
-    <motion.div drag dragConstraints={refrence} className="card w-[20vw]  bg-slate-600/35 h-[40vh] p-5 rounded-[25px] text-gray-300 shadow-lg cursor-pointer relative overflow-hidden">
-      <div className="content">
+    <motion.div
+      drag
+      dragConstraints={refrence}
+      className={`card w-[20vw]  bg-slate-600/35 h-[40vh] p-5 rounded-[25px] text-gray-300 shadow-lg cursor-pointer relative overflow-hidden ${
+        checked ? "bg-green-400/35" : ""
+      }`}
+    >
+      <div className="content ">
         <FaRegFile />
-        <p className="font-thin text-[1.2vw] leading-tight my-4 overflow-y-auto max-h-[22.5vh] overflow-x-hidden">
+        <p
+          className={`font-thin text-[1.2vw] leading-tight my-4 overflow-y-auto max-h-[22.5vh] overflow-x-hidden ${
+            checked ? "line-through" : ""
+          }`}
+        >
           {data.task}
         </p>
       </div>
       <div className="buttons flex items-center justify-center gap-10 absolute bottom-0 left-0 text-center w-full p-4">
-        <input type="checkbox" className="scale-150" />
+        <input
+          type="checkbox"
+          onChange={() => {
+            setChecked((prev) => !prev);
+          }}
+          className="scale-150"
+        />
         <span className="text-[3vw]">
           <MdEdit />
         </span>
--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -5,18 +5,19 @@ function Foreground() {
   const refrence = useRef(null);
 
   let todos = [
-    { task: "Complete homework", checked: true },
-    { task: "Go for a run", checked: false },
-    { task: "Buy groceries", checked: true },
-    { task: "Call mom", checked: false },
-  ];
+    { id: 1, task: "Complete homework", checked: true },
+    { id: 2, task: "Go for a run", checked: false },
+    { id: 3, task: "Buy groceries", checked: true },
+    { id: 4, task: "Call mom", checked: false },
+    { id: 5, task: "Study for exam", checked: false },
+];
   return (
     <div
       ref={refrence}
       className="foreground w-full h-screen absolute p-5 flex gap-11 justify-center flex-wrap"
     >
-      {todos.map((item, index)=>(
-        <Card refrence={refrence} data={item}/>
+      {todos.map((item, index) => (
+        <Card refrence={refrence} data={item} />
       ))}
     </div>
   );
